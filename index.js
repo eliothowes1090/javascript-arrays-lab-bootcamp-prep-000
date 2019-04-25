29e3a09ac19270e7026566cbc15dbdf094aff1f9
Eliot Howes <ejhowes@gmail.com>
2019-04-25 15:43:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function prependKitten(name) {
   };
 
   function removeLastKitten() {
-    var newKittens = kittens.slice((kittens.length - 1), kittens.length)
+    var newKittens = kittens.slice(kittens.length - 1)
     return newKittens
   };
